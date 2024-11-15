895e60bdd2c67af70a6fc960692f117204d470f0
makiwebdeveloper <makiwebdeveloper@gmail.com>
2024-11-15 23:34:10 +0100

    simple blockchain (hash / add block / init blockchain)

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/makiwebdeveloper/maki-blockchain/blockchain"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First block after Genesis")
+	chain.AddBlock("Second block after Genesis")
+	chain.AddBlock("Third block after Genesis")
+
+	for _, block := range chain.Blocks {
+		// fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %x\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
 }
